164ec9db1a19f8c66580ae7067e2de3c71aaac9b
Kenisha Vaswani <kenishavaswani@Kenishas-MacBook-Pro.local>
2024-02-10 15:24:30 -0800

    kv - created fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItem.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneMenu: {
+        "id": 1,
+        "diningCommonsCode": "ortega",
+        "name": "burger",
+        "station": "cafe"
+    },
+    threeDates: [
+        {
+            "id": 1,
+            "diningCommonsCode": "ortega",
+            "name": "burger",
+            "station": "cafe"
+        },
+        {
+            "id": 2,
+            "diningCommonsCode": "ortega",
+            "name": "chips",
+            "station": "grill"
+        },
+        {
+            "id": 1,
+            "diningCommonsCode": "portola",
+            "name": "pizza",
+            "station": "table"
+        }
+    ]
+};
+
+
+export { ucsbDatesFixtures };
