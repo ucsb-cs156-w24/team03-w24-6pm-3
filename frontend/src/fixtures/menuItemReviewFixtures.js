f0a93990014e85de425c66e0d3b0782e409c7030
Andy Jin <zhengyangjin@ucsb.edu>
2024-02-14 14:58:47 -0800

    Create menuItemReviewFixtures.js

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,35 @@
+const menuItemReviewFixtures = {
+    oneReview: {
+        "id": 1,
+        "itemId": "1",
+        "stars": 1,
+        "dateReviewed": "2024-02-14T22:30:25",
+        "comments": "bad"
+    },
+    threeReviews: [
+        {
+            "id": 1,
+            "itemId": "1",
+            "stars": 2,
+            "dateReviewed": "2024-03-14T22:30:25",
+            "comments": "gooooood"
+        },
+        {
+            "id": 2,
+            "itemId": "1",
+            "stars": 3,
+            "dateReviewed": "2024-02-14T22:30:25",
+            "comments": "goooooood"
+        },
+        {
+            "id": 3,
+            "itemId": "1",
+            "stars": 4,
+            "dateReviewed": "2024-02-14T22:30:25",
+            "comments": "gooood"
+        }
+    ]
+};
+
+
+export { ucsbmenuItemReviewFixtures };
